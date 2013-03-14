d2bd1dba0fd16f4b6005ccb5ea12becbc9625dda
nsatvik <nsatvik@gmail.com>
2013-03-14 17:58:39 +0530

    Initial Project Import
    I declare this project wide open! Let the coding begin

---
+++ TSDM/src/org/ck/gui/MainClass.java
@@ -0,0 +1,8 @@
+package org.ck.gui;
+
+public class MainClass {
+	public static void main(String args[]){
+		System.out.println("I declare this project open");
+		System.out.println("Open Rangnam Style! :)");
+	}
+}
