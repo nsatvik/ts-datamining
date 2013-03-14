161562b51558c4b7fe88cdf61e541a206c81d281
nsatvik <nsatvik@gmail.com>
2013-03-14 18:19:12 +0530

    Proper Import :)

--- TSDM/src/org/ck/gui/MainClass.java
+++
@@ -1,8 +0,0 @@
-package org.ck.gui;
-
-public class MainClass {
-	public static void main(String args[]){
-		System.out.println("I declare this project open");
-		System.out.println("Open Rangnam Style! :)");
-	}
-}
