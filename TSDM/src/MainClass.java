884ef06e9e3d4fad2321494825a8f9cb9027622d
vaishakhbn <vaishakh.bargurreddy@gmail.com>
2013-03-14 18:28:40 +0530

    I declare the project OPEN Rangnam Style!
    With Ganesha's blessing and harlem shake :D

--- TSDM/src/MainClass.java
+++ TSDM/src/MainClass.java
@@ -5,5 +5,8 @@ public class MainClass
 	public static void main(String args[])
 	{
 		System.out.println("Hello,World!");
+		System.out.println("I delare the project WIDE Open!");
+		System.out.println("We first start this program with Ganesha's blessing along with Rangman Style and Harlem Shakes :P");
+		
 	}
 }
